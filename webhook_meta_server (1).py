2c016473f679be42cee7abea2f137f0e910df51d
Vhee Dev <vhee.dev@gmail.com>
2025-04-28 21:39:31 +0700

    Add files via upload

---
+++ webhook_meta_server (1).py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = 'mygentlevision2025'  # Token ini harus sama dengan yang di Meta Developer Settings
+
+@app.route('/', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'GET':
+        token_sent = request.args.get('hub.verify_token')
+        challenge = request.args.get('hub.challenge')
+        if token_sent == VERIFY_TOKEN:
+            return challenge
+        return 'Invalid verification token', 403
+    elif request.method == 'POST':
+        return '200 OK', 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
